a86c4dbe77fbe3b6efb3f5947feec16cfb47f704
KareemCodes <ksasa1999@gmail.com>
2020-06-16 11:21:29 -0500

    defined functions

---
+++ patient_billing.py
@@ -0,0 +1,27 @@
+import os
+
+path = os.getcwd() + '\\patients\\'
+
+def patient_options():
+    return 0
+
+def create_database():
+    return 0
+
+def add_patient():
+    return 0
+
+def read_patient():
+    return 0
+
+def update_patient():
+    return 0
+
+def update_information():
+    return 0
+
+def delete_patient():
+    return 0
+
+def total_patients():
+    return 0
