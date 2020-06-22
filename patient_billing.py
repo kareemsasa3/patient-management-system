bcf09543998afb8639929642769efad71c7b32f5
kareemsasa3 <kareemsasa3@gmail.com>
2020-06-21 20:27:10 -0500

    created patient options function

--- patient_billing.py
+++ patient_billing.py
@@ -3,6 +3,34 @@ import os
 path = os.getcwd() + '\\patients\\'
 
 def patient_options():
+    print("Enter one of the following actions: ")
+    print("A - Add a new patient")
+    print("R - Read a patient record")
+    print("U - Update a patient's information")
+    print("D - Delete a patient")
+    print("T - Display total number of patients")
+
+    user_input = input("= ")
+
+    if user_input in ('A', 'a'):
+        add_patient()
+    elif user_input in ('R', 'r'):
+        read_patient()
+    elif user_input in ('U', 'u'):
+        update_patient()
+    elif user_input in ('D', 'd'):
+        delete_patient()
+    elif user_input in ('T', 't'):
+        total_patients()
+    else:
+        print("\nUser entered character with no patient options\n")
+        patient_options()
+
+    print()
+    continue_program = input("Would you like to do anything else? (Y or N): ")
+    if continue_program in ('Y', 'y'):
+        print()
+        patient_options()
     return 0
 
 def create_database():
